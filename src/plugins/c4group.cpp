af820be97f3f92b511eee35e3ef1af5d116d7044
Fulgen301 <tokmajigeorge@gmail.com>
2018-06-24 18:31:15 +0200

    c4group: Fix broken fileRead() implementation, move tree creation to the right callback

--- src/plugins/c4group.cpp
+++ src/plugins/c4group.cpp
@@ -5,7 +5,6 @@
 #include <QFileInfo>
 #include <QString>
 #include <QSet>
-#include <QMutexLocker>
 #include "c4group.h"
 
 void C4GroupPlugin::init(LCEdit *editor)
@@ -15,35 +14,38 @@ void C4GroupPlugin::init(LCEdit *editor)
 
 ExecPolicy C4GroupPlugin::createTree(const QDir &base, LCTreeWidgetItem *parent)
 {
+	if (parent == nullptr)
+		return EP_Continue;
+	if (parent->childCount() == 0)
+	{
+		QString suffix = QFileInfo(parent->filePath()).completeSuffix();
+		if (QSet<QString>({"c4d", "c4f", "c4s", "c4p"}).contains(suffix))
+		{
+			C4Group grp;
+			try
+			{
+				openChildGroup(&grp, parent->filePath());
+			}
+			catch (QString e)
+			{
+				return parent->childCount() ? EP_AbortMain : EP_Continue;
+			}
+
+			char filename[_MAX_PATH + 1];
+			grp.FindEntry("*", filename);
+			while (grp.FindNextEntry("*", filename))
+			{
+				m_editor->createEntry<LCTreeWidgetItem>(parent, QString(filename), QDir(parent->filePath()).filePath(filename));
+			}
+
+			return EP_AbortMain;
+		}
+	}
 	return EP_Continue;
 }
 
 ExecPolicy C4GroupPlugin::treeItemChanged(LCTreeWidgetItem *current, LCTreeWidgetItem *previous)
 {
-	if (current == nullptr)
-		return EP_Continue;
-	
-	if (current->childCount() == 0)
-	{	QString suffix = QFileInfo(current->filePath()).completeSuffix();
-		if (QSet<QString>({"c4d", "c4f", "c4s", "c4p"}).contains(suffix))
-		{
-			QMutexLocker locker(&m_editor->mutex);
-			
-			// C4Group's path is the root folder where we reside, so we need to access the entry first
-			C4Group grp;
-			openChildGroup(&grp, current->filePath());
-			
-			char filename[_MAX_PATH + 1];
-			grp.FindEntry("*", filename);
-			assert(filename);
-			while (grp.FindNextEntry("*", filename))
-			{
-				m_editor->createEntry<LCTreeWidgetItem>(current, QString(filename), QDir(current->filePath()).filePath(filename));
-			}
-			
-			return EP_AbortMain;
-		}
-	}
 	return EP_Continue;
 }
 
@@ -53,10 +55,10 @@ void C4GroupPlugin::openChildGroup(C4Group* grp, const QString& path)
 	{
 		grp->Open(TO_CSTR(GetFirstExistingPath(QFileInfo(path))));
 	}
-	
+
 	QString notOpenedPath = path.right(path.length() - grp->GetFullName().getLength());
 	notOpenedPath = notOpenedPath.remove(0, 1);
-	
+
 	foreach(const QString &part, notOpenedPath.split(QDir::separator(), QString::SkipEmptyParts))
 	{
 		if (!grp->OpenChild(TO_CSTR(part)))
@@ -88,15 +90,21 @@ ReturnValue<QByteArray> C4GroupPlugin::fileRead(LCTreeWidgetItem* item, off_t of
 			return ReturnValue<QByteArray>();
 		}
 	}
-	
+
 	grp.Advance(offset);
-	size_t realSize = size ? size : grp.AccessedEntrySize();
+	size_t realSize = size ? qMin<size_t>(size, grp.AccessedEntrySize()) : grp.AccessedEntrySize();
+	if (offset + realSize > grp.AccessedEntrySize())
+		realSize = static_cast<size_t>(qMax<int>(grp.AccessedEntrySize() - offset, 0)); // size_t is unsigned, so qMax<size_t> fails if one argument is negative
+
+	if (realSize == 0)
+		return ReturnValue<QByteArray>(EP_AbortAll, QByteArray());
+
 	char buffer[realSize];
 	grp.Read(buffer, realSize);
 	return ReturnValue<QByteArray>(EP_AbortAll, QByteArray(buffer, realSize));
 }
 
-ReturnValue<int> C4GroupPlugin::fileWrite(LCTreeWidgetItem *item, const QByteArray &buf)
+ReturnValue<int> C4GroupPlugin::fileWrite(LCTreeWidgetItem *item, const QByteArray &buf, off_t offset)
 {
 	return ReturnValue<int>(EP_Continue, 0);
 }
--- src/plugins/c4group.h
+++ src/plugins/c4group.h
@@ -11,15 +11,15 @@ class C4GroupPlugin : public QObject, public LCPluginInterface
 	Q_OBJECT
 	Q_PLUGIN_METADATA(IID LCPlugin_Iid)
 	Q_INTERFACES(LCPluginInterface)
-	
+
 public:
 	void init(LCEdit *editor) override;
 	ExecPolicy createTree(const QDir & base, LCTreeWidgetItem * parent) override;
 	int priority() override;
 	ExecPolicy treeItemChanged(LCTreeWidgetItem * current, LCTreeWidgetItem * previous) override;
 	ReturnValue<QByteArray> fileRead(LCTreeWidgetItem *item, off_t offset = 0, size_t size = 0) override;
-	ReturnValue<int> fileWrite(LCTreeWidgetItem *item, const QByteArray &buf) override;
-	
+	ReturnValue<int> fileWrite(LCTreeWidgetItem *item, const QByteArray &buf, off_t offset = 0) override;
+
 private:
 	void openChildGroup(C4Group *grp, const QString &path);
 	LCEdit *m_editor;
