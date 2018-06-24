3705b180dc0da14c94fae26b9700f1199d906097
Fulgen301 <tokmajigeorge@gmail.com>
2018-06-24 18:32:46 +0200

    texteditor: Fix file saving, add syntax highlighting

--- src/plugins/texteditor.cpp
+++ src/plugins/texteditor.cpp
@@ -1,3 +1,8 @@
+#include <QMimeType>
+#include <QMimeDatabase>
+#include <QHash>
+#include <QUrl>
+#include <QApplication>
 #include "texteditor.h"
 
 ExecPolicy TextEditorPlugin::createTree(const QDir &base, LCTreeWidgetItem *parent)
@@ -10,7 +15,7 @@ ReturnValue<QByteArray> TextEditorPlugin::fileRead(LCTreeWidgetItem *item, off_t
 	return ReturnValue<QByteArray>();
 }
 
-ReturnValue<int> TextEditorPlugin::fileWrite(LCTreeWidgetItem *item, const QByteArray &buf)
+ReturnValue<int> TextEditorPlugin::fileWrite(LCTreeWidgetItem *item, const QByteArray &buf, off_t offset)
 {
 	return ReturnValue<int>(EP_Continue, 0);
 }
@@ -20,9 +25,16 @@ void TextEditorPlugin::init(LCEdit *editor)
 	m_editor = editor;
 	m_texteditor = KTextEditor::Editor::instance();
 	m_document = m_texteditor->createDocument(this);
-	m_textview = m_document->createView(editor);
-	editor->ui->layRight->addWidget(m_textview);
+	m_textview = m_document->createView(m_editor);
+	m_editor->ui->layRight->addWidget(m_textview);
 	m_textview->hide();
+}
+
+TextEditorPlugin::~TextEditorPlugin()
+{
+//	SAFE_DELETE(m_textview)
+	SAFE_DELETE(m_document)
+//	SAFE_DELETE(m_texteditor)
 }
 
 int TextEditorPlugin::priority()
@@ -32,20 +44,43 @@ int TextEditorPlugin::priority()
 
 ExecPolicy TextEditorPlugin::treeItemChanged(LCTreeWidgetItem *current, LCTreeWidgetItem *previous)
 {
-	if (current && QSet<QString>({"txt", "c", "log"}).contains(QFileInfo(current->text(0)).completeSuffix()))
+	SAFE_DELETE(watcher)
+	if (current)
 	{
-		QByteArray text = m_editor->fileRead(current, 0, 0);
-		m_document->setText(QString(text));
-		m_editor->ui->lblName->hide();
-		m_editor->ui->txtDescription->hide();
-		m_textview->show();
-		return EP_AbortAll;
+		QFileInfo info(current->text(0));
+//		QByteArray text = m_editor->fileRead(current, 0, 24);
+//		QMimeType type = QMimeDatabase().mimeTypeForFileNameAndData(file.fileName(), text);
+		if (QSet<QString>({"c", "log", "txt", "c4m"}).contains(info.completeSuffix()))
+		{
+			file.open();
+			file.write(m_editor->fileRead(current, 0, 0));
+			file.resize(file.pos());
+			file.close();
+
+			watcher = new QFileSystemWatcher();
+			watcher->addPath(file.fileName());
+			QObject::connect(watcher, &QFileSystemWatcher::fileChanged, [this, current](const QString &path)
+			{
+				if (path == file.fileName())
+				{
+					QFile f(path);
+					f.open(QIODevice::ReadOnly | QIODevice::Text);
+					QByteArray buf = f.readAll();
+					f.close();
+					m_editor->fileWrite(current, buf, 0);
+				}
+			});
+			m_document->openUrl(QUrl::fromLocalFile(file.fileName()));
+			m_document->setHighlightingMode(m_document->highlightingModes().contains(info.baseName()) ? info.baseName() : info.completeSuffix() == "c" ? "C4Script" : "Normal");
+			m_editor->ui->lblName->hide();
+			m_editor->ui->txtDescription->hide();
+			m_textview->show();
+			return EP_AbortAll;
+		}
 	}
-	else
-	{
-		m_textview->hide();
-		m_editor->ui->lblName->show();
-		m_editor->ui->txtDescription->show();
-		return EP_Continue;
-	}
+
+	m_textview->hide();
+	m_editor->ui->lblName->show();
+	m_editor->ui->txtDescription->show();
+	return EP_Continue;
 }
--- src/plugins/texteditor.h
+++ src/plugins/texteditor.h
@@ -4,6 +4,8 @@
 #undef LineFeed
 
 #include <QDir>
+#include <QFileSystemWatcher>
+#include <QTemporaryFile>
 #include <KTextEditor/Document>
 #include <KTextEditor/Editor>
 #include <KTextEditor/View>
@@ -15,19 +17,21 @@ class TextEditorPlugin : public QObject, public LCPluginInterface
 	Q_OBJECT
 	Q_PLUGIN_METADATA(IID LCPlugin_Iid)
 	Q_INTERFACES(LCPluginInterface)
-	
+
 public:
+	~TextEditorPlugin() override;
 	void init(LCEdit *editor) override;
 	ExecPolicy createTree(const QDir & base, LCTreeWidgetItem * parent) override;
 	int priority() override;
 	ExecPolicy treeItemChanged(LCTreeWidgetItem * current, LCTreeWidgetItem * previous) override;
 	ReturnValue<QByteArray> fileRead(LCTreeWidgetItem *item, off_t offset, size_t size) override;
-	ReturnValue<int> fileWrite(LCTreeWidgetItem *item, const QByteArray &buf) override;
-	
+	ReturnValue<int> fileWrite(LCTreeWidgetItem *item, const QByteArray &buf, off_t offset = 0) override;
+
 private:
-	KTextEditor::Editor *m_texteditor;
-	KTextEditor::View *m_textview;
-	KTextEditor::Document *m_document;
+	KTextEditor::Editor *m_texteditor = nullptr;
+	KTextEditor::View *m_textview = nullptr;
+	KTextEditor::Document *m_document = nullptr;
 	LCEdit *m_editor;
-	
+	QTemporaryFile file;
+	QFileSystemWatcher *watcher = nullptr;
 };
