bbcf7f331cb13a1c11c4b714d1626e512aa8e4f5
Fulgen301 <tokmajigeorge@gmail.com>
2019-01-18 20:22:43 +0100

    Travis: Add before_script: script

---
+++ scripts/travis_before_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ "$TRAVIS_OS_NAME" = "linux"]
+then
+	sudo add-apt-repository -y ppa:beineri/opt-qt-5.11.0-xenial
+	sudo apt-get update
+	sudo apt-get install qt511base qt511tools qt511svg
+fi
